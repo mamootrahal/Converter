70c1f45f82df9a50007317faa0098fe9d1b7e5fd
CLenin <36728380+CLenin@users.noreply.github.com>
2018-09-30 18:02:10 +0300

    Files

--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -12,9 +12,23 @@ namespace Converter
 {
     public partial class Form1 : Form
     {
+        int bin, hex;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                bin = Convert.ToInt32(textBox1.Text);
+            }
         }
     }
 }
